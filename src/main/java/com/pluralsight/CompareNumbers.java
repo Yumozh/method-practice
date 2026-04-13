a039904d1b1bd44b5652618112c5d88436553718
Yumozh <juliiamozhchil@gmail.com>
2026-04-13 13:24:28 -0500

    feat: check if number is positive

--- src/main/java/com/pluralsight/CompareNumbers.java
+++ src/main/java/com/pluralsight/CompareNumbers.java
@@ -2,10 +2,23 @@ package com.pluralsight;
 
 public class CompareNumbers {
     public static void main(String[] args) {
-    System.out.println(isEven(5));
+        System.out.println(isEven(5));
+        System.out.println(isPositive(9));
     }
-    private static boolean isEven (int number){
-        if (number % 2 = 0) {
-            return true;
-        }
+
+    private static boolean isEven(int number) {
+//        if (number % 2 == 0) {
+//            return true;
+//        }else{
+//            return false;
+//        }
+        boolean result = number % 2 == 0;
+
+        return result;
+    }
+
+    private static boolean isPositive(double number){
+        return number > 0;
+    }
 }
+
