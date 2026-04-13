9938f03144bd6c2e0ae4c0dc5e0804a59e11975c
Yumozh <juliiamozhchil@gmail.com>
2026-04-13 13:07:23 -0500

    feat: in new class check if number is even

---
+++ src/main/java/com/pluralsight/CompareNumbers.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class CompareNumbers {
+    public static void main(String[] args) {
+    System.out.println(isEven(5));
+    }
+    private static boolean isEven (int number){
+        if (number % 2 = 0) {
+            return true;
+        }
+}
